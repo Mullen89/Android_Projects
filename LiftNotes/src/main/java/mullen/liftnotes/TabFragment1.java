b47578e3a931255055ea0d5551ea22f3476219be
Eric Mullen <ericmullen1989@gmail.com>
2018-10-28 20:17:20 -0500

    Add files via upload

--- LiftNotes/src/main/java/mullen/liftnotes/TabFragment1.java
+++ LiftNotes/src/main/java/mullen/liftnotes/TabFragment1.java
@@ -1,26 +1,33 @@
 package mullen.liftnotes;
 
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.text.InputType;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+        import android.app.AlertDialog;
+        import android.app.ListFragment;
+        import android.content.DialogInterface;
+        import android.os.Bundle;
+        import android.support.annotation.Nullable;
+        import android.support.v4.app.Fragment;
+        import android.support.v7.widget.RecyclerView;
+        import android.text.InputType;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.ArrayAdapter;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.ListView;
+        import android.widget.Toast;
 
-import java.util.ArrayList;
+        import java.util.ArrayList;
 
 public class TabFragment1 extends Fragment {
 
-    private Button addWorkout;
-    private String mText = "";
-    private ArrayList<String> workoutList = new ArrayList<String>();
+    Button addWorkout;
+    String mText = "";
+    ArrayList<String> workoutList = new ArrayList<String>();
+    ArrayAdapter<String> workoutListAdapter;
+    ListView listViewer;
+
 
     public TabFragment1() {
         // Required empty public constructor
@@ -29,9 +36,11 @@ public class TabFragment1 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable
-                             Bundle savedInstanceState) {
+            Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.tab_fragment1, container, false);
+        workoutListAdapter = new ArrayAdapter<String>(getActivity(), R.layout.workout_item_layout, workoutList);
+        listViewer = (ListView) view.findViewById(R.id.workoutListView);
 
         addWorkout = (Button) view.findViewById(R.id.addWorkoutBtn);
         addWorkout.setOnClickListener(new View.OnClickListener() {
@@ -42,9 +51,11 @@ public class TabFragment1 extends Fragment {
             }
         });
 
+        listViewer.setAdapter(workoutListAdapter);
         return view;
     }
 
+    //Alert Dialog Interface box pop-up to add workout to workoutList<>
     private void addWorkoutToList() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this.getContext());
         builder.setTitle("Name of Workout");
@@ -58,6 +69,7 @@ public class TabFragment1 extends Fragment {
             public void onClick(DialogInterface dialog, int which) {
                 mText = input.getText().toString();
                 workoutList.add(mText);
+                workoutListAdapter.notifyDataSetChanged();
             }
         });
         builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
