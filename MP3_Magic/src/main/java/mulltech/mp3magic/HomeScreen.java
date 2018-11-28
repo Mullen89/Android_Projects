6d86935f411d3ca8bde1610cdca5b6bd5105336c
Eric Mullen <ericmullen1989@gmail.com>
2018-11-27 19:52:50 -0600

    Add files via upload

---
+++ MP3_Magic/src/main/java/mulltech/mp3magic/HomeScreen.java
@@ -0,0 +1,13 @@
+package mulltech.mp3magic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen);
+    }
+}
