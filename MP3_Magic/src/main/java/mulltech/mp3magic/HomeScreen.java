e298198cc91582f58fcb8a34e1769cfb08f7daf0
Eric Mullen <ericmullen1989@gmail.com>
2018-11-29 20:52:31 -0600

    Add files via upload

--- MP3_Magic/src/main/java/mulltech/mp3magic/HomeScreen.java
+++ MP3_Magic/src/main/java/mulltech/mp3magic/HomeScreen.java
@@ -2,12 +2,36 @@ package mulltech.mp3magic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HomeScreen extends AppCompatActivity {
+
+    private Button convertBtn;
+    private TextView urlTV;
+    public String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
+
+        /**
+         * Grabs string URL address from text box
+         */
+        urlTV = (TextView) findViewById(R.id.urlTV);
+        url = urlTV.getText().toString();
+
+        /**
+         * 
+         */
+        convertBtn = (Button) findViewById(R.id.btnConvert);
+        convertBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
