0dc6a6ba66b7ca004c4a3f070abd95277a73941b
Eric Mullen <ericmullen1989@gmail.com>
2018-12-01 23:17:02 -0600

    Add files via upload

--- MP3_Magic/src/main/java/mulltech/mp3magic/HomeScreen.java
+++ MP3_Magic/src/main/java/mulltech/mp3magic/HomeScreen.java
@@ -24,7 +24,7 @@ public class HomeScreen extends AppCompatActivity {
         url = urlTV.getText().toString();
 
         /**
-         * 
+         *
          */
         convertBtn = (Button) findViewById(R.id.btnConvert);
         convertBtn.setOnClickListener(new View.OnClickListener() {
