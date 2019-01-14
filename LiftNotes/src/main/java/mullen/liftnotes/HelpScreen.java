6c212e1542eaa914d4b7a3101f8131395e8f080d
Eric Mullen <ericmullen1989@gmail.com>
2019-01-13 23:25:12 -0600

    Add files via upload

---
+++ LiftNotes/src/main/java/mullen/liftnotes/HelpScreen.java
@@ -0,0 +1,13 @@
+package mullen.liftnotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelpScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help_screen);
+    }
+}
