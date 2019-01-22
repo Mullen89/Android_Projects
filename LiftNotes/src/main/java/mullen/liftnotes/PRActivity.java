0cf04ee4f3019fd3f81d640e5c956ddc30322f1e
Eric Mullen <ericmullen1989@gmail.com>
2019-01-21 21:55:33 -0600

    Add files via upload

--- LiftNotes/src/main/java/mullen/liftnotes/PRActivity.java
+++ LiftNotes/src/main/java/mullen/liftnotes/PRActivity.java
@@ -2,12 +2,23 @@ package mullen.liftnotes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class PRActivity extends AppCompatActivity {
 
+    private ImageButton prBackBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pr);
+
+        prBackBtn = (ImageButton) findViewById(R.id.prHistBckBtn);
+        prBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
